46383c9f21b8502cf1dd5ed76988b6aea0d89d82
Nekko6 <matheus.hbz432@gmail.com>
2025-11-12 21:45:56 -0300

    AuroraV1.01
    mudança do comandos para os slash e /safada e puro meme

--- AuroraBotV1.0.py
+++ AuroraBotV1.0.py
@@ -1,49 +1,64 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='.', intents=intents)
 
-# isso permite ligar a aurora
+##### EVENTOS #####
+
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name} - {bot.user.id}')
+    sincs = await bot.tree.sync()
+    print(f"{len(sincs)} comandos sincronizados.")
+    print(f'Bom dia Aurora {bot.user.name} - {bot.user.id}')
 
-# esse evento permite que a aurora de boas vindas para novos membros
 @bot.event
-async def on_member_join(member:discord.Member):
+async def on_member_join(member: discord.Member):
     canal = bot.get_channel(1280444833600897096)
     await canal.send(f"🔔 Tring tring! 🔔\n{member.mention} é o nosso novo cliente🧋")
+
     join_embed = discord.Embed()
     join_embed.title = "Bem-vindo ao Servidor!"
     join_embed.description = "Estamos felizes em ter você aqui. Aproveite sua estadia!"
-    join_embed.set_image(url="https://i.pinimg.com/originals/bf/88/a9/bf88a9cad16364149a4e307d66659870.gif ")
+    join_embed.set_image(
+        url="https://i.pinimg.com/originals/bf/88/a9/bf88a9cad16364149a4e307d66659870.gif"
+    )
+
     await canal.send(embed=join_embed)
 
-# dando esse comando a aurora responde com uma saudação
-@bot.command()
-async def ola(ctx:commands.Context):
-    nome = ctx.author.name
-    await ctx.reply(f"Olá:blue_heart: , {nome} Eu sou o AuroraBot, como posso ajudar você hoje?")
 
-#aqui a aurora da uma lista dos comandos
-@bot.command()
-async def ajuda(ctx:commands.Context):
-    ajuda_mensagem = (
-        "Aqui estão alguns comandos que você pode usar\n"
-        ".ola - O bot responde com uma saudação.\n"
-        ".ajuda - Mostra esta mensagem de ajuda.\n"
-        "Mais comandos serão adicionados em breve!"
+##### TREE COMANDOS #####
+
+@bot.tree.command()
+async def ola(interaction: discord.Interaction):
+    await interaction.response.send_message(
+        f'Olá🩵, {interaction.user.mention}! Como posso ajudar você hoje?'
     )
-    await ctx.reply(ajuda_mensagem)
 
-#aqui eu testo a embed
+
+@bot.tree.command()
+async def ajuda(interaction: discord.Interaction):
+    await interaction.response.send_message(
+        "Os meus comandos são esses:\n"
+        "/ola → Eu digo oi 👋\n"
+        "/ajuda → Mostra os comandos\n"
+    )
+
+
+@bot.tree.command()
+async def safada(interaction: discord.Interaction):
+    await interaction.response.send_message(f'{interaction.user.mention}, sua safada!')
+
+
+##### COMANDOS COMUNS #####
+
 @bot.command()
-async def join_embed(ctx:commands.Context):
-   join_embed = discord.Embed()
-   join_embed.title = "Bem-vindo ao Servidor!"
-   join_embed.description = "Estamos felizes em ter você aqui. Aproveite sua estadia!"
-  
-#isso permite rodar a aurora
-bot.run("")
+async def join_embed(ctx: commands.Context):
+    join_embed = discord.Embed()
+    join_embed.title = "Bem-vindo ao Servidor!"
+    join_embed.description = "Estamos felizes em ter você aqui. Aproveite sua estadia!"
+    await ctx.reply(embed=join_embed)
 
+bot.run("seu token
+")
